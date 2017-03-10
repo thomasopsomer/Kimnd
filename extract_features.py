7e454d111cea2b674ff2f9b4e9f8fefb2d9ab616
Chaïmaa Kadaoui <chaimaa.kadaoui@gmail.com>
2017-03-10 14:46:38 +0100

    textual features

--- extract_features.py
+++ extract_features.py
@@ -231,10 +231,15 @@ if __name__=="__main__":
     list_recipients = np.unique(train_df['recipient'].tolist())
     for ind, row in train_df_not_flat.iterrows():
         mid = row['mid']
-        df_all_outgoing = pd.DataFrame(columns=['mid', 'user', 'contact', 'outgoing text'])
+        df_all_outgoing = pd.DataFrame(columns=['mid', 'user', 'contact', 'outgoing_text'])
         for user in list_sender:
             df_all_outgoing.append(textual_features.outgoing_text_similarity(
-                train_df_not_flat, mid, user, idf, id2word, avg_len, 5)
+                train_df_not_flat, mid, user, idf, id2word, avg_len, n)
+            )
+        df_all_incoming = pd.DataFrame(columns=['mid', 'user', 'contact', 'incoming'])
+        for user in list_recipients:
+            df_all_incoming.append(textual_features.incoming_text_similarity(
+                train_df_not_flat, mid, user, idf, id2word, avg_len, n)
             )
 
 
